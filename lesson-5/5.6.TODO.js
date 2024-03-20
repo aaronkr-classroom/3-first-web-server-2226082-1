7c68c7100d47e04ea96de7e88fdb53b0c3604247
2226082 <162406741+2226082@users.noreply.github.com>
2024-03-20 06:16:35 +0000

    2226082 강민성  5번

--- lesson-5/5.6.TODO.js
+++ lesson-5/5.6.TODO.js
@@ -7,12 +7,33 @@
  */
 
 // 요청에 따른 라우트의 매핑 정의
-const routeRespMap;
+const routeRespMap = {
+    "/": "<h1>Home Page</h1><p>Welcome to my page!</p>",
+    "/": "<h1>About page</h1><p>About Me!</p>",
+    "/": "<h1>Info Page</h1><p>Personal Info</p>",
+  
+   
+    
+};
 
 // listing5.5.js에서 (p. 96)
-const port;
-
+const port = 3000,
+    http = require('http'),
+    httpstatus = require('http-status-codes'),
+    app = http.createServer();
+    app.on("request", (req, res) => {
+        res.writeHead(httpstatus.OK,{
+            "content-Type": "text/html",
+        });
 // 요청 라우트가 정의된 맵에 있는지 체크
+if(routeRespMap[req.url]) { // "/about"
+    setTimeout(() => {
+        res.end(routeRespMap[req.url]);
+    }, 2000);
+    
+        
+}
+
 
 // <<< 나머진 서버 코드 입력 하십시오 >>>
 
